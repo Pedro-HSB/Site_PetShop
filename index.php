288693340995d09c2b6ef6d539685b00d9766099
Matheus Ferreira <matheus.ferreira359@etec.sp.gov.br>
2022-04-03 16:53:35 -0300

    Primeiro commit do projeto. Foram criadas apenas os corpo e o .css para formaatação do site

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <meta name="viewport" content="width=device-width">
+ 	<meta name="keywords" content="pet">
+ 	<meta name="description" content="Site de PetShop">
+ 	<link rel="stylesheet" type="text/css" href="formatacao.css">
+ 	<style type="text/css">
+  <title>PetsCão<center></title></center>
+
+
+</head>
+</style>
+</head>
+<body>
+
+<div id = corpo>
+<div id=pos> 
+<img id=marca src="logo.PNG">
+</div>
+   <p><center>O melhor lugar para o seu companheiro</center></p>
+  
+ <div id = texto>
+ <h1><center>Aqui na PestCão você encontra tudo que há de melhor para o seu Pet! Confira nossos serviços:</center></h1> 
+</div>
+
+<div id= servicos>
+<h3><li id= bt><a href=>Banho e Tosa</a></li> </h3>
+<h3><li id= bt><a href=>Consultas</a></li> </h3>
+<h3><li id= bt><a href=>Produtos</a></li> </h3>
+<h3><li id= bt><a href=>Contatos</a></li> </h3>
+
+</div>
+</div>
+</body></html>
